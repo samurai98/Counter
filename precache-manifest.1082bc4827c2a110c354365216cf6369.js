a7f9fe56de25600122292cab3dbfeae3b8adacdd
samurai98 <kirito1498@gmail.com>
2020-07-01 19:00:59 +0300

    Updates

---
+++ precache-manifest.1082bc4827c2a110c354365216cf6369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595c21a89a21cb2cad51f3bbb8bf13b2",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "bbe7e2cb11ddd8dc68c9",
+    "url": "/Counter/static/css/main.4d4c6185.chunk.css"
+  },
+  {
+    "revision": "2c988c6a173cf64fa122",
+    "url": "/Counter/static/js/2.2b1d6f3f.chunk.js"
+  },
+  {
+    "revision": "bbe7e2cb11ddd8dc68c9",
+    "url": "/Counter/static/js/main.75953acc.chunk.js"
+  },
+  {
+    "revision": "cd6e87301d7e441c1fae",
+    "url": "/Counter/static/js/runtime~main.130d950d.js"
+  }
+]);
